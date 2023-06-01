34674127d3d69d8e5813f23a09656e4a3502ac27
HempQuarterz <hempquarterz@gmail.com>
2023-06-01 11:32:27 -0700

    JavaScript Lab 5.1 Functions

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+////////////////////////////////////////
 // 1. 
 
 // function maxOfTwoNumbers(x, y) {
@@ -9,21 +10,68 @@
 // }
 // console.log(maxOfTwoNumbers(3, 9));
 
+////////////////////////////////
 //2.
 
-const maxOfThree = function(x, y, z) {
-    let max = x;
+// const maxOfThree = function(x, y, z) {
+//     let max = x;
 
-    if (y > max) {
-        max = y;
-    }
-    if (z > max) {
-        max = z;
-    }
+//     if (y > max) {
+//         max = y;
+//     }
+//     if (z > max) {
+//         max = z;
+//     }
 
-    return max;
-}
- 
-console.log(maxOfThree(3,12,9));
+//     return max;
+// } 
+// console.log(maxOfThree(3,12,9));
 
+////////////////////////////////////////////////////////////////
+//3. 
+
+// function isCharAVowel(char) {
+//     const vowels = ['a','e','i','o','u','y']; 
+//     const lowerCaseChar = char.toLowerCase();
     
+//     return vowels.includes(lowerCaseChar);   
+// }
+// console.log(isCharAVowel('A'));
+
+////////////////////////////////////////////////////////////////
+//4. 
+
+// const sumArray = function(a,b,c) {
+//     let sum = a + b + c;
+//     return sum; 
+// };
+
+// console.log(sumArray(5, 10, 15));
+
+////////////////////////////////
+//5.
+
+// function multiplyArray(a,b,c) {
+//     let product = a * b * c;
+//     return product;
+// }
+// console.log(multiplyArray(5, 10, 2));
+
+//////////////////////////////////////
+//6. 
+
+// const numArgs = function() {
+//     return arguments.length;
+// };
+
+// console.log(numArgs('Testing', 'Testing', 1, 2, 3));
+
+////////////////////////////////
+//7.
+
+// function reverseString(str) {
+//     return str.split('').reverse().join('');
+// }
+
+// console.log(reverseString('rockstar'));
+
